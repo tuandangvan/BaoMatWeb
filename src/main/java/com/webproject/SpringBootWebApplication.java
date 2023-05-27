bf67188af2947a36d198170dd246ddf8c203036b
Tú meow meow <84791711+tumeomeo@users.noreply.github.com>
2023-05-28 01:08:45 +0700

    `X-Content-Type-Options Header Missing`

--- src/main/java/com/webproject/SpringBootWebApplication.java
+++ src/main/java/com/webproject/SpringBootWebApplication.java
@@ -5,13 +5,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 import com.webproject.config.StorageProperties;
+import com.webproject.config.WebSecurityConfig;
 import com.webproject.service.StorageService;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
+@Import(WebSecurityConfig.class)
 public class SpringBootWebApplication {
 
 	public static void main(String[] args) {
